0095406b208a818b23ed33a22229d8dc77c59f4b
NagKothapalli <nag022@gmail.com>
2022-12-15 23:34:02 +0530

    Add project files.

---
+++ ConsoleAppOne/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleAppOne
+{
+    //Modifier[opt]   class   Name{  body   }
+    public class Program
+    {
+        //Body  - Members ->  variables  / functions
+
+        //Modifier[opt]    DataType    Name ;  ->Declaration           //Modifier[opt]    DataType    Name  =  value; ->Assignment
+
+        //student -> rollnum , percentge ,  grade , result , name , collegeName
+
+        public static int stdNumber = 22; //rice bag - 20 Kg capacity
+
+        public long population = 2275676577677657;
+
+        private double percentage = 88.56;
+
+        internal char grade = 'A';  //a to z -> a b c d e f g h i .... 26
+
+        protected Boolean result = true;  //true false
+
+        string stdName = "Ram";    // Typemismatch error
+
+        public static string collegeName = "JNTU";
+
+        public long number = stdNumber; //TypeCasting ->Implicit typecasting , Widening
+
+
+
+
+    }
+}
