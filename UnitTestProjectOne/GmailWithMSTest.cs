1aaf334a1db181def58a01d25b89da2270bc7838
NagKothapalli <nag022@gmail.com>
2023-01-06 08:41:23 +0530

    Jan6th code pushed

--- UnitTestProjectOne/GmailWithMSTest.cs
+++ UnitTestProjectOne/GmailWithMSTest.cs
@@ -17,43 +17,100 @@ namespace UnitTestProjectOne
         [TestMethod]   //@Test
         public  void ComposeAndSendAnEmail() // 0 0 
         {
-            //Debug.WriteLine("Login to Application - Debug ");
-            Console.WriteLine("Test Case : Compose And Send An Email - Console");
             Debug.WriteLine("Test Case : Compose And Send An Email - Debug ");
+            LaunchApplication();
+            LoginToApplication();
+            Compose();
+            Send();
+            LogoutFromApplication();
+            CloseApplication();
         }
         [TestMethod]  // ifcondition   , ifelse  , elseif , switchcase , nestedif 
         public  void ReplyToAnEmail() // 0 0 
         {
-            //Debug.WriteLine("Login to Application - Debug ");
-            Console.WriteLine("Test Case : Reply to An Email - Console");
             Debug.WriteLine("Test Case : Reply to An Email - Debug ");
+            LaunchApplication();
+            LoginToApplication();
+            Open();
+            Reply();
+            LogoutFromApplication();
+            CloseApplication();
         }
         [TestMethod]
         public  void ForwardAnEmail() // 0 0 
         {
-            //Debug.WriteLine("Login to Application - Debug ");
-            Console.WriteLine("Test Case : Forward An Email - Console");
             Debug.WriteLine("Test Case : Forward An Email - Debug ");
+            LaunchApplication();
+            LoginToApplication();
+            Open();
+            Forward();
+            LogoutFromApplication();
+            CloseApplication();
         }
         [TestMethod]
         public  void DeleteAnEmail() // 0 0 
         {
-            //Debug.WriteLine("Login to Application - Debug ");
-            Console.WriteLine("Test Case : Delete An Email - Console");
             Debug.WriteLine("Test Case : Delete An Email - Debug ");
+            LaunchApplication();
+            LoginToApplication();
+            Open();
+            Delete();
+            LogoutFromApplication();
+            CloseApplication();
         }
 
-        [TestInitialize]   //@Before
-        public void Login()
+        // [TestInitialize]   //@Before
+        //******************ReusableComponents********************
+        private void LaunchApplication()
         {
-            Debug.WriteLine("Login to Application - Debug - From Intialize ");
+            Debug.WriteLine("Launch the Application  ");
+            // 10 lines of selenium code  
+            //open an empty browser
+            //call the application url
+            //maximize the browser
+            //verifiy the URL
+            //verifiy the title of application
+            //verify the home page
         }
-        [TestCleanup] //@After
-        public void Logout()
+        void LoginToApplication()
         {
-            Debug.WriteLine("Logout From Application - Debug - From Intialize ");
+            Debug.WriteLine("Login to Application  ");
+           // 10 lines of selenium code
         }
-
+        //[TestCleanup] //@After
+        private void LogoutFromApplication()
+        {
+            Debug.WriteLine("Logout From Application ");
+        }
+        public void CloseApplication()
+        {
+            Debug.WriteLine("Close the Application ");
+        }
+        void Compose()
+        {
+            Debug.WriteLine("Compose the Email ");
+        }
+        public void Send()
+        {
+            Debug.WriteLine("Send the Email ");
+        }
+        protected void Open()
+        {
+            Debug.WriteLine("Open the Email ");
+        }
+        public void Reply()
+        {
+            Debug.WriteLine("Reply to Email ");
+        }
+        protected void Forward()
+        {
+            Debug.WriteLine("Compose the Email ");
+        }
+        public void Delete()
+        {
+            Debug.WriteLine("Delete the Email ");
+        }
+        //******************************************************
         public void Addition()
         {
 
