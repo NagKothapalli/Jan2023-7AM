d6359a295f2f8a6f84830e3bfdb9baa44c23eefc
NagKothapalli <nag022@gmail.com>
2023-02-15 09:01:38 +0530

    Feb15th code pushed

---
+++ BDDWithSpecflow/Features/Calculator.feature
@@ -0,0 +1,24 @@
+﻿Feature: Calculator
+![Calculator](https://specflow.org/wp-content/uploads/2020/09/calculator.png)
+Simple calculator for adding **two** numbers
+
+Link to a feature: [Calculator](BDDWithSpecflow/Features/Calculator.feature)
+***Further read***: **[Learn more about how to generate Living Documentation](https://docs.specflow.org/projects/specflow-livingdoc/en/latest/LivingDocGenerator/Generating-Documentation.html)**
+
+
+
+@smoke
+Scenario: Subtract two numbers
+ Given user gave the first number
+ And user gave the second number
+ When the two numbers are subtracted
+ Then validate the result
+
+@mytag
+Scenario: Add two numbers
+	Given the first number is 50
+	And the second number is 70
+	When the two numbers are added
+	Then the result should be 120
+
+
---
+++ BDDWithSpecflow/Steps/CalculatorStepDefinitions.cs
@@ -0,0 +1,78 @@
+﻿using System.Diagnostics;
+using TechTalk.SpecFlow;
+
+namespace BDDWithSpecflow.Steps
+{
+    [Binding]
+    public class CalculatorStepDefinitions
+    {
+
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        private readonly ScenarioContext _scenarioContext;
+
+        public CalculatorStepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        //******************************
+        int fnumber;
+        int snumber;
+        int diff;
+        [Given(@"user gave the first number")]
+        public void GivenUserGaveTheFirstNumber()
+        {
+            fnumber = 22;
+            Debug.WriteLine("First Number:" +fnumber);
+        }
+
+        [Given(@"user gave the second number")]
+        public void GivenUserGaveTheSecondNumber()
+        {
+            snumber = 12;
+            Debug.WriteLine("Second Number:" + fnumber);
+        }
+
+        [When(@"the two numbers are subtracted")]
+        public void WhenTheTwoNumbersAreSubtracted()
+        {
+            diff = fnumber - snumber;
+            Debug.WriteLine("Perform Subtraction");
+        }
+
+        [Then(@"validate the result")]
+        public void ThenValidateTheResult()
+        {
+            Debug.WriteLine("Difference of two numbers :" + diff);
+        }
+
+
+
+        //***********************************
+
+        [Given("the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int number)
+        {
+            
+        }
+
+        [Given("the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int number)
+        {
+           
+        }
+
+        [When("the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            
+        }
+
+        [Then("the result should be (.*)")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            
+        }
+    }
+}
